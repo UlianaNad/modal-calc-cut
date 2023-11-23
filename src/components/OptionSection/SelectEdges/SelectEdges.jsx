008e1e924e691e2e19662a46481e3d42eb8d65f7
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-11-23 16:05:01 +0200

    fixed

--- src/components/OptionSection/SelectEdges/SelectEdges.jsx
+++ src/components/OptionSection/SelectEdges/SelectEdges.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import EdgePreview from './EdgePreview/EdgePreview';
 
 const SelectEdges = ({ handleSelectChange, handleInputCheckboxChange }) => {
